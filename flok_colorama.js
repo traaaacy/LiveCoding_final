d1b2554b1ac34594ab665fc65a22ae7ffac57e2e
traaaacy <109930793+traaaacy@users.noreply.github.com>
2024-05-02 17:02:17 +0400

    Update flok_colorama.js

--- flok_colorama.js
+++ flok_colorama.js
@@ -1,4 +1,4 @@
-let video = document.createElement('video');
+video = document.createElement('video');
 video.loop = true;
 video.crossOrigin = "anonymous";
 video.autoplay = true;
@@ -8,8 +8,8 @@ s0.init({ src: video, dynamic: true });
 src(s0).out(o0)
 }
 
-let video1 = document.createElement('video');
-video1.loop = false;
+ video1 = document.createElement('video');
+video1.loop = true;
 video1.crossOrigin = "anonymous";
 video1.autoplay = true;
 video1.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXdpcnA1ZGwwOHh1M3VvdHlwdWJrZndxMjE5MDBicjF4N3BnYm0xdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7rgvqNIjeERE6YYuIm/giphy.mp4';
@@ -18,10 +18,9 @@ s1.init({ src: video1, dynamic: true });
 src(s1).out(o1)
 }
 
-render(o1)
 
-let video2 = document.createElement('video');
-video2.loop = false;
+ video2 = document.createElement('video');
+video2.loop = true;
 video2.crossOrigin = "anonymous";
 video2.autoplay = true;
 video2.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGo1cGp1cWJ1czl2OTFjeDFrYmVxemNuY2hzdWdzMzh2djIxMzR3NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OO6TCSWMap2Xjqiefp/giphy.mp4'
@@ -30,8 +29,8 @@ s2.init({ src: video2, dynamic: true });
 src(s2).out(o2)
 }
 
-let video3 = document.createElement('video');
-video3.loop = false;
+video3 = document.createElement('video');
+video3.loop = true;
 video3.crossOrigin = "anonymous";
 video3.autoplay = true;
 video3.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTMycTVrcjVtcHB4ZzQyc2J6c2JzZDQ0Z2J1M3J6bWZ1bGRxNndpeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dOa1aggZxfHbqaHVwk/giphy.mp4';
@@ -41,8 +40,8 @@ src(s3).out(o3)
 }
 
 
-let video4 = document.createElement('video');
-video4.loop = false;
+ video4 = document.createElement('video');
+video4.loop = true;
 video4.crossOrigin = "anonymous";
 video4.autoplay = true;
 video4.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXllYnA5ZHV4NzBiejAyemEycHlyNW0yZ29oaGRuN29samEyeGRtaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Zp5AIKVxPIsJSjFR2e/giphy.mp4';
@@ -51,8 +50,8 @@ s4.init({ src: video4, dynamic: true });
 src(s4).out(o4)
 }
 
-let video5 = document.createElement('video');
-video5.loop = false;
+video5 = document.createElement('video');
+video5.loop = true;
 video5.crossOrigin = "anonymous";
 video5.autoplay = true;
 video5.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjZucjdkdG9tcWVsbnF6MHNmcnQ2bGNiYXpiMW5kaG9yZ3NtcDJtZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HKqj34hDeSNRPczQ2o/giphy.mp4';
@@ -61,8 +60,8 @@ s5.init({ src: video5, dynamic: true });
 src(s5).out(o5)
 }
 
-let video6 = document.createElement('video');
-video6.loop = false;
+video6 = document.createElement('video');
+video6.loop = true;
 video6.crossOrigin = "anonymous";
 video6.autoplay = true;
 video6.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHUyajhpNm5oNHllZHNudzdhZzY2eGd3Mm8zcHBzcHlqOHFzaTUzcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cyHu7cXZOgGraRHyGj/giphy.mp4';
@@ -71,8 +70,8 @@ s6.init({ src: video6, dynamic: true });
 src(s6).out(o6)
 }
 
-let video7 = document.createElement('video');
-video7.loop = false;
+video7 = document.createElement('video');
+video7.loop = true;
 video7.crossOrigin = "anonymous";
 video7.autoplay = true;
 video7.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbm5saXY5dmUzbjc1MnIzdGlhcjFubWNwaGU5bDVmajExaXk5NnYxYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Q32UxRozozEsCPrG0Z/giphy.mp4';
@@ -81,13 +80,10 @@ s7.init({ src: video7, dynamic: true });
 src(s7).out(o7)
 }
 
-render(o4)
-
 
 
 osc(50,0.1,5).out(o3)
 
-render(o3)
 
 
 
@@ -98,8 +94,6 @@ osc(20, 0.2, 0)
 .out(o0)
 
 hush()
-
-render(o1)
 
 
 osc(20, 0.2, 0)
@@ -136,7 +130,7 @@ p5.hide() // REMEMBER TO DO HIDE!
 
 //
 
-let mode =2;
+let mode =4;
 p5.draw = ()=>{
   if (mode==1){
   shader1.loop= true;
@@ -169,6 +163,8 @@ s8.init({src: p5.canvas})
 src(s8).out(o8)
 //
 
+hush()
+
 
 
 src(o8).layer(src(o3).luma(0.3))
@@ -178,9 +174,19 @@ src(o8).layer(src(o3).luma(0.3))
   // .repeat(2, 2)
   .blend(shape(7, [0.01, 0.1].smooth()).scrollY(3, 0.3).scrollX(Math.random(-200), -0.1).luma(0.5,0.1), 0.2)
   // .modulate(noise(2, 0.6))
-  .colorama(0.3)
-  //.scrollX(1, 0.2)
+  .colorama(0.2)
+  //.colorama(0.3)
+  //.colorama(0.1)
+  //.scrollX(1, -0.2)
   .out(o9)
+video.currentTime=0;
+video1.currentTime=0;
+video2.currentTime=0;
+video3.currentTime=0;
+video4.currentTime=0;
+video5.currentTime=0;
+video6.currentTime=0;
+video7.currentTime=0;
 
 
 render(o9)
